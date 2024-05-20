c647d6aa4856193f0ff5538c03021b7150cf8852
Lala <lalaaagh@code.edu.az>
2024-05-20 16:13:51 +0400

    first commit

---
+++ CardApp-Storyboard/Card.swift
@@ -0,0 +1,19 @@
+//
+//  Card.swift
+//  CardApp-Storyboard
+//
+//  Created by Lala on 20.05.24.
+//
+
+import UIKit
+
+struct Card {
+    static var allValues = [
+        UIImage(named: "pic1")!,
+        UIImage(named: "pic2")!,
+        UIImage(named: "pic3")!,
+        UIImage(named: "pic4")!,
+        UIImage(named: "pic5")!,
+        UIImage(named: "pic6")!
+    ]
+}
